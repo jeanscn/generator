38d793ba61c0dbf9fc00f39026a09ba39370896d
jeanscn <cjj@vip.sina.ocm>
2022-03-03 23:03:44 +0800

    完善前端生成

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/db/DatabaseIntrospector.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/db/DatabaseIntrospector.java
@@ -1,18 +1,3 @@
-/**
- *    Copyright 2006-2020 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.mybatis.generator.internal.db;
 
 import org.mybatis.generator.api.FullyQualifiedTable;
@@ -24,7 +9,6 @@ import org.mybatis.generator.api.dom.java.JavaReservedWords;
 import org.mybatis.generator.config.*;
 import org.mybatis.generator.internal.ObjectFactory;
 import org.mybatis.generator.internal.util.JavaBeansUtil;
-import org.mybatis.generator.internal.util.StringUtility;
 import org.mybatis.generator.logging.Log;
 import org.mybatis.generator.logging.LogFactory;
 
@@ -60,14 +44,6 @@ public class DatabaseIntrospector {
         logger = LogFactory.getLog(getClass());
     }
 
-    private void calculateForeignKey(FullyQualifiedTable table,IntrospectedTable introspectedTable){
-        //通过table属性配置获得，非物理表外键
-        String foreignKeyColumnProperty = introspectedTable.getConfigPropertyValue("selectByColumn", PropertyScope.table);
-        if (StringUtility.stringHasValue(foreignKeyColumnProperty)) {
-            String[] split = foreignKeyColumnProperty.split(",");
-            Arrays.stream(split).forEach(introspectedTable::addForeignKeyColumn);
-        }
-    }
 
     private void calculatePrimaryKey(FullyQualifiedTable table,IntrospectedTable introspectedTable) {
         ResultSet rs = null;
@@ -637,8 +613,6 @@ public class DatabaseIntrospector {
             }
 
             calculatePrimaryKey(table, introspectedTable);
-
-            calculateForeignKey(table, introspectedTable);
 
             enhanceIntrospectedTable(introspectedTable);
 
