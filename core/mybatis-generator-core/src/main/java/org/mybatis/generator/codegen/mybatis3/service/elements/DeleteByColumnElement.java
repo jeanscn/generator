f83fcc1bb8b035e9ff011d80c33fb0c4f7af5fe7
jeanscn <cjj@vip.sina.com>
2025-02-13 07:09:38 +0800

    调整update返回0时为正常

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/service/elements/DeleteByColumnElement.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/service/elements/DeleteByColumnElement.java
@@ -1,5 +1,6 @@
 package org.mybatis.generator.codegen.mybatis3.service.elements;
 
+import com.vgosoft.core.constant.enums.core.EntityEventEnum;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.codegen.mybatis3.service.AbstractServiceElementGenerator;
@@ -26,14 +27,32 @@ public class DeleteByColumnElement extends AbstractServiceElementGenerator {
     @Override
     public void addElements(TopLevelClass parentElement) {
 
+        boolean containPreDeleteEvent = this.serviceImplConfiguration.getEntityEvent().contains(EntityEventEnum.PRE_DELETE.name());
+        boolean containDeletedEvent = this.serviceImplConfiguration.getEntityEvent().contains(EntityEventEnum.DELETED.name());
+
         String params = configuration.getColumns().stream()
                 .map(column -> column.getJavaProperty() + (configuration.getParameterList() ? "s" : ""))
                 .collect(Collectors.joining(","));
         Method methodByColumn = serviceMethods.getDeleteByColumnMethod(parentElement, configuration, false);
         methodByColumn.addAnnotation("@Override");
-        String sb = "return mapper." + configuration.getDeleteMethodName() +
-                "(" + params +  ");";
-        methodByColumn.addBodyLine(sb);
+        if (containPreDeleteEvent || containDeletedEvent) {
+            methodByColumn.addBodyLine("int ret = 0;");
+            methodByColumn.addBodyLine(" List<{0}> items = mapper.{1}({2});",entityType.getShortName(),configuration.getMethodName(),params);
+            methodByColumn.addBodyLine("if (!items.isEmpty()) {");
+            if (containPreDeleteEvent) {
+                methodByColumn.addBodyLine("publisher.publishEvent(items,EntityEventEnum.{0});", EntityEventEnum.PRE_DELETE.name());
+            }
+            methodByColumn.addBodyLine("ret = mapper.{0}({1});",configuration.getDeleteMethodName(),params);
+            if (containDeletedEvent) {
+                methodByColumn.addBodyLine("publisher.publishEvent(items,EntityEventEnum.{0});",EntityEventEnum.DELETED.name());
+            }
+            methodByColumn.addBodyLine("}");
+            methodByColumn.addBodyLine("return ret;");
+        }else{
+            String sb = "return mapper." + configuration.getDeleteMethodName() +
+                    "(" + params +  ");";
+            methodByColumn.addBodyLine(sb);
+        }
         parentElement.addMethod(methodByColumn);
     }
 }
